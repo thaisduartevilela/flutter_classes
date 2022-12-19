fab3fd0108c25e87c4243406131320a349692002
Thais Duarte Vilela <81780589+thaisduartevilela@users.noreply.github.com>
2022-12-19 10:15:14 -0300

    Modificações Scaffolf e stateless

--- Stateless_Stateful/lib/main.dart
+++ Stateless_Stateful/lib/main.dart
@@ -17,11 +17,45 @@ class MyApp extends StatelessWidget {
       ),
       home: Scaffold(
         appBar: AppBar(
-          title: Text('task organizer'),
+          title: Text('Task Organizer'),
         ),
-        body: Container(color: Colors.black,),
-        floatingActionButton: FloatingActionButton(onPressed:(){}),
+        body: Container(
+          child: Stack(
+            children: [
+              Container(
+                color: Colors.blue,
+  height: 140,
+              ),
+              Container(
+                  color: Colors.white30,
+                  height: 100,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        color: Colors.black26,
+                        width: 72,
+                        height: 100,
+                      ),
+                      Text('Aprender flutter'),
+                      ElevatedButton(
+                          onPressed: () {}, child: Icon(Icons.arrow_drop_up))
+                    ],
+                  )),
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(onPressed: () {}),
       ),
     );
+  }
+}
+
+class  extends StatelessWidget {
+  const ({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
   }
 }
